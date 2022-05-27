41d82745b2923939f6ef75199b4821280ed6010e
wjdsantos <walter4asantos@gmail.com>
2022-05-27 16:22:06 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/trabcap01srt3/TrabCap01Srt3Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.trabcap01srt3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabCap01Srt3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabCap01Srt3Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/trabcap01srt3/TrabCap01Srt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.trabcap01srt3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabCap01Srt3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
