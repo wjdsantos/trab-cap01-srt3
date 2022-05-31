cca6b6aa370d04d6bdd761e7a07af874034d7c61
wjdsantos <walter4asantos@gmail.com>
2022-05-31 15:06:44 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/trabcap01srt3/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/trabcap01srt3/services/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.trabcap01srt3.entities.Client;
 import com.devsuperior.trabcap01srt3.repositories.ClientRepository;
@@ -14,6 +15,7 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Client> finAll() {
 		return repository.findAll();
 	}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João Alberto', '00122233311', 5200.0, TIMESTAMP WITH TIME ZONE '1990-07-14T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Antonio Carlos', '00223334422', 4100.0, TIMESTAMP WITH TIME ZONE '1995-05-27T07:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Cezar Augusto', '00324435533', 6000.0, TIMESTAMP WITH TIME ZONE '1991-03-12T15:00:00Z', 2);
